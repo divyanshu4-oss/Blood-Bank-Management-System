f052098c018db0709e9d2bd619013513046a307d
divyanshu kumar <80243571+divyanshu4-oss@users.noreply.github.com>
2021-12-11 17:04:27 +0530

    Delete dbconnect.py

--- dbconnect.py
+++
@@ -1,9 +0,0 @@
-# Creating table into database!!!
-import sqlite3
-
-# Connect to sqlite database
-def getsqliteconnection():
-    conn = sqlite3.connect('bms.sqlite')
-    # conn = sqlite3.connect('proj.db')
-    print("Connected to SQLite")
-    return conn
